1f62c61a95cb21e5b745063f60379911c940d5d8
exam <exam@Bcalab4-m15>
2024-04-27 09:20:10 +0530

    commit 1

---
+++ src/question1/House_114.java
@@ -0,0 +1,16 @@
+package question1;
+
+public class House_114 {
+	String color;
+	String Owner;
+	
+	House_114(String color,String owner) {
+		this.color=color;
+		this.Owner=owner;
+	}
+	
+	String getOwner() {
+		return Owner;
+	}
+
+}
